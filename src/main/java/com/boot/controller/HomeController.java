d9d6477fc9ecfa9047256f13469f9fd09d545eaa
uresh <uresh88@gmail.com>
2019-08-08 23:10:40 +0530

    FIrst boot application lesson

---
+++ src/main/java/com/boot/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.boot.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @RequestMapping("/")
+    public String name(){
+        return "Uresh first blog";
+    }
+}
